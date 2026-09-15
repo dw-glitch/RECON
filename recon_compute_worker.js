0e9d81cc27173ea9605fa88a753f26ee3991335e
dw-glitch <soudebararaka@gmail.com>
2026-09-15 08:43:18 -0300

    fix(allocation): load master path governance in worker

--- recon_compute_worker.js
+++ recon_compute_worker.js
@@ -7,7 +7,7 @@ let bootError = null;
 try {
   importScripts(
     "recon_contracts.js", "core.js", "ld_conflicts.js", "allocation_confirmation_sources.js",
-    "allocation_core.js", "databook_allocation_sources.js", "non_tagged_title_rules.js",
+    "allocation_core.js", "allocation_path_governance.js", "databook_allocation_sources.js", "non_tagged_title_rules.js",
     "document_title_standard.js", "audit_core.js", "timeline_core.js", "relations_core.js", "tag_conference_core.js"
   );
 } catch (error) {
